aac922d8c6eaa39145d6ef232890d01a9ce90016
AritificialPhysics <sanskar10100@gmail.com>
2020-08-25 23:32:28 +0530

    Added credential encryption function to helper module.

--- utils/helper.py
+++ utils/helper.py
@@ -1,9 +1,19 @@
 """Provides various helper functions."""
 
 import tkinter as tk
+import hashlib
 
 def create_button(frame, button_text, function_to_call):
+	"""Returns a new button created according to the style guidelines."""
 	return tk.Button(master=frame, text=button_text, fg='#FFFFFF', bg='#009688', command=function_to_call)
 
 def create_label(frame, label_text):
+	"""Returns a newly created label according to the style guidlines."""
 	return tk.Label(master=frame, text=label_text, fg='#212121', bg='#CFD8DC')
+
+def encrypt_credentials(username, password):
+	"""Return the login credential in sha256 encrypted format."""
+	encrypt_login_details = list()
+	for credential in login_details:
+		encrypt_login_details.append(hashlib.sha256(credential.encode()).hexdigest())
+	return tuple(encrypt_login_details)
