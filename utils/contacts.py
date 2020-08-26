8eee22c901d160bd46dc962926deadb2745f907f
AritificialPhysics <sanskar10100@gmail.com>
2020-08-27 00:31:57 +0530

    added show all contacts

--- utils/contacts.py
+++ utils/contacts.py
@@ -7,6 +7,11 @@ import tkinter as tk
 from tkinter import messagebox
 from . import user
 from . import helper
+import sqlite3
+
+
+conn = sqlite3.connect('material.db')
+c = conn.cursor()
 
 
 class ContactsManagement:
@@ -19,6 +24,8 @@ class ContactsManagement:
 		self.tablename = tablename
 		self.window = window
 		self.frame = frame
+		self.clicked = tk.IntVar()
+		self.clicked.set(0)
 		self.draw_contacts_menu()
 
 	def _gen_new_frame(self):
@@ -38,7 +45,26 @@ class ContactsManagement:
 		helper.create_button(self.frame, text='Search Contact', command=self.search_contact).grid()
 
 	def show_all_contacts(self):
-		pass
+		"""Shows all contacts for a user, along with the contact count."""
+		# The 0th entry in the tuple returned by fetchone contains the count
+		self._gen_new_frame()
+		contact_count = c.execute(f'SELECT COUNT(*) FROM {self.tablename}').fetchone()[0]
+		# First print the contact count
+		textbox = tk.Text(self.frame, bg='#455A64', fg='#FFFFFF', height=1)
+		textbox.insert(tk.END, f'Contacts count: {contact_count}')
+		textbox.grid()
+		# Make a textbox for each row
+		for name, number, email in c.execute(f'SELECT * FROM {self.tablename}'):
+			textbox = tk.Text(master=self.frame, bg='#455A64', fg='#FFFFFF', height=1)
+			textbox.insert(tk.END, f'{name} | {number} | {email}')
+			textbox.grid()
+		else:
+			# Go back when button pressed
+			btn_go_back = helper.create_button(self.frame, 'Go Back', command=lambda: self.clicked.set(1))
+			btn_go_back.grid(column=0, sticky='w')
+			btn_go_back.wait_variable(self.clicked)
+			self.draw_contacts_menu()
+
 
 	def add_contact(self):
 		pass
